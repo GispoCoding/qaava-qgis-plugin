6d2f4b2ed719b1a04db77ed7482e9fc19e34e701
Joonalai <joona@gispo.fi>
2020-08-18 09:19:20 +0300

    #38: Add copyright and license notice

--- Qaava/test/test_db_tools.py
+++ Qaava/test/test_db_tools.py
@@ -1,3 +1,38 @@
+#  Gispo Ltd., hereby disclaims all copyright interest in the program Qaava-qgis-plugin
+#  Copyright (C) 2020 Gispo Ltd (https://www.gispo.fi/).
+#
+#
+#  This file is part of Qaava-qgis-plugin.
+#
+#  Qaava-qgis-plugin is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  Qaava-qgis-plugin is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with Qaava-qgis-plugin.  If not, see <https://www.gnu.org/licenses/>.
+#
+#
+#  This file is part of Qaava-qgis-plugin.
+#
+#  Qaava-qgis-plugin is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  Qaava-qgis-plugin is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with Qaava-qgis-plugin.  If not, see <https://www.gnu.org/licenses/>.
+
 import pytest
 
 from .conftest import (remove_db_settings, CONN_NAME, QGIS_APP)
